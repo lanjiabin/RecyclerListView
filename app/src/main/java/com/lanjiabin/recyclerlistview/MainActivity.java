b9ab513f05c9311126d5b862df94ae39a833483e
lanjiabin <1323457707@qq.com>
2020-02-21 01:24:28 +0800

    添加header和footer 1.添加常用颜色 2.添加header和footer 3.修改了item_recyclerview的名字 4.添加详细的备注 5.注意添加header和footer后list与recyclerview的position错位问题 6.header和footer的布局引用直接在adaptr完成 7.注意notifyItemRemoved(position)的位置问题，更新不存在的位置会报错

--- app/src/main/java/com/lanjiabin/recyclerlistview/MainActivity.java
+++ app/src/main/java/com/lanjiabin/recyclerlistview/MainActivity.java
@@ -20,8 +20,12 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerAdapter mRecyclerAdapter;
     private List<String> mList = new ArrayList<>();
     private Button addDataButton, changeData;
-    //记录用户选择了RecyclerListView哪个item
-    private int mPosition;
+
+    /**
+     * 记录用户选择了RecyclerListView哪个item
+     * 赋值-1是为了防止默认选中position=0的item
+     */
+    private int mPosition=-1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +40,35 @@ public class MainActivity extends AppCompatActivity {
         changeData = findViewById(R.id.changeData);
         mRecyclerView = findViewById(R.id.recyclerListView);
 
+        //添加数据到选择item的上方
         addDataButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //添加数据到选择item的上方
-                mRecyclerAdapter.addData(mPosition);
+                /**
+                 * 因为添加了header和footer，所以mPosition的值是从-1到 mList.size()+1
+                 * 头部和尾部的item是不应该支持修改数据的，因为用的是不同的布局，数据也不是相同的
+                 * */
+                if (mPosition>=0 && mPosition<mList.size()){
+                    mRecyclerAdapter.addData(mPosition);
+                    mRecyclerAdapter.notifyDataSetChanged();
+                }
+
             }
         });
 
-
+        //修改选中item的数据
         changeData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //修改选中item的数据
-                mRecyclerAdapter.changeData(mPosition);
+                /**
+                 * 因为添加了header和footer，所以mPosition的值是从-1到 mList.size()+1
+                 * 头部和尾部的item是不应该支持修改数据的，因为用的是不同的布局，数据也不是相同的
+                 * */
+                if (mPosition>=0 && mPosition<mList.size()){
+                    mRecyclerAdapter.changeData(mPosition);
+                    mRecyclerAdapter.notifyDataSetChanged();
+                }
+
             }
         });
 
@@ -59,8 +78,8 @@ public class MainActivity extends AppCompatActivity {
     public void initRecyclerView() {
 
         //添加测试数据
-        for (int i = 0; i < 110; i++) {
-            mList.add(i + " ");
+        for (int i = 0; i < 20; i++) {
+            mList.add(i + "-----");
         }
         //设置布局方向-默认布局
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -72,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.addItemDecoration(new RecycleViewDivider(this, LinearLayoutManager.HORIZONTAL));
 
         //设置adapter关联的list
-        mRecyclerAdapter = new RecyclerAdapter(this, mList);
+        mRecyclerAdapter = new RecyclerAdapter(this, mList,mRecyclerView);
 
         //利用GestureDetector手势检测类来实现回调
         mRecyclerView.addOnItemTouchListener(new RecyclerViewClickListener(this, mRecyclerView, new RecyclerViewClickListener.OnItemClickListener() {
@@ -83,25 +102,32 @@ public class MainActivity extends AppCompatActivity {
                 mRecyclerAdapter.notifyDataSetChanged();
 
                 //记录用户选择了哪个item
-                mPosition = position;
+                mPosition = position-1;
 
                 //传递点击了哪个item，以此用来更新选中item的颜色
                 mRecyclerAdapter.getCurrentPosition(position);
 
                 Toast.makeText(MainActivity.this, "单击了：" + position, Toast.LENGTH_SHORT).show();
-
             }
 
             @Override
             public void onItemLongClick(View view, int position) {
-                Toast.makeText(MainActivity.this, "长击了：" + position, Toast.LENGTH_SHORT).show();
-                //删除长按的item
-                mRecyclerAdapter.removeData(position);
+                /**
+                 * 因为添加了header和footer，所以mPosition的值是从-1到 mList.size()+1
+                 * 头部和尾部的item是不应该支持修改数据的，因为用的是不同的布局，数据也不是相同的
+                 * */
+                if (position>0 && position<=mList.size()){
+                    //长按删除的item
+                    mRecyclerAdapter.removeData(position);
+                }
+
+                Toast.makeText(MainActivity.this, "长击了：" + (position), Toast.LENGTH_SHORT).show();
             }
         }));
 
         //关联adapter
         mRecyclerView.setAdapter(mRecyclerAdapter);
     }
+
 }
 
--- app/src/main/java/com/lanjiabin/recyclerlistview/RecyclerAdapter.java
+++ app/src/main/java/com/lanjiabin/recyclerlistview/RecyclerAdapter.java
@@ -15,14 +15,28 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     private List<String> mList;
     private Context mContext;
-
-    //记录用户选择了RecyclerListView哪个item
-    //赋值-1是为了防止默认选中position=0的item
+    /**
+     * 记录用户选择了RecyclerListView哪个item
+     * 赋值-1是为了防止默认选中position=0的item
+     */
     private int mPosition = -1;
 
-    public RecyclerAdapter(Context context, List<String> list) {
+    public static final int TYPE_HEADER = 0;  //说明是带有Header的
+    public static final int TYPE_FOOTER = 1;  //说明是带有Footer的
+    public static final int TYPE_NORMAL = 2;  //说明是不带有header和footer的
+
+    /**
+     * mHeaderView 头部
+     * mFooterView 尾部
+     */
+    private View mHeaderView;
+    private View mFooterView;
+
+    public RecyclerAdapter(Context context, List<String> list, RecyclerView recyclerView) {
         this.mContext = context;
         this.mList = list;
+        this.mHeaderView = LayoutInflater.from(mContext).inflate(R.layout.header_recylerview, recyclerView, false);
+        this.mFooterView = LayoutInflater.from(mContext).inflate(R.layout.footer_recylerview, recyclerView, false);
     }
 
     //获取当前点击item的position
@@ -32,7 +46,30 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     //移除数据
     public void removeData(int position) {
-        mList.remove(position);
+        /**
+         * 这里要重点讲解一下；
+         * 添加了header和footer之后；
+         * header的位置就是recyclerView是在position为0位置，footer是在position为recyclerView.size()-1的位置；
+         * 那数据list真正占用的位置就是recyclerView的 1 --> recyclerView.size()-2 位置
+         * 当点击recyclerView的footer的前一个位置的时候（recyclerView.size()-2）
+         * 得到的recyclerView的position为recyclerView.size()+1，而list的position为list.size()
+         * 位置差了一个1,所以在传递最后一个数据点击删除的时候，会数组越界
+         * 举例：position指的是下标
+         * list的长度为10，那数据的position就是 0 ~ 9；
+         * 加入header和footer后，recyclerView的position=0 的位置就是header；
+         * position=11 的位置就是footer；
+         * list的 position=0 的数据放在recyclerView的 position=1 的位置；
+         * list的 position=9 的数据放在recyclerView的 position=10 的位置；
+         * 长按recyclerView的 position=10的位置的时候，传递的方法remove(position)中的position=10；
+         * 传递的是recyclerView的position，就成了 mList.remove(10);
+         * mList.remove(10)，明显数组越界了，因为mList最大的position为9；
+         * 所以在删除最后一个数据的时候，要特别处理，就是下面的方法。
+         * 重点是notifyItemRemoved(position);方法
+         * 删除的position - 1处的list数据
+         * 而更新的是recyclerView的position的位置
+         * 因为他们错位了 1
+         * */
+        mList.remove(position - 1);
         notifyItemRemoved(position);
     }
 
@@ -48,6 +85,22 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         notifyItemChanged(position);
     }
 
+    //获取item的view的类型
+    @Override
+    public int getItemViewType(int position) {
+
+        if (mHeaderView == null && mFooterView == null) {
+            return TYPE_NORMAL;
+        }
+        if (position == 0 && mHeaderView != null) {
+            return TYPE_HEADER;
+        }
+        if (position == getItemCount() - 1 && mFooterView != null) {
+            return TYPE_FOOTER;
+        }
+        return TYPE_NORMAL;
+    }
+
     /**
      * 该方法会在RecyclerView需要展示一个item的时候回调，
      * 重写该方法，使ViewHolder加载item的布局，布局复用，提高性能，
@@ -57,10 +110,18 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+        if (mHeaderView != null && viewType == TYPE_HEADER) {
+            return new MyViewHolder(mHeaderView);
+
+        }
+        if (mFooterView != null && viewType == TYPE_FOOTER) {
+            return new MyViewHolder(mFooterView);
+        }
+
         MyViewHolder holder = new MyViewHolder(
                 LayoutInflater.from(mContext)
                         .inflate(
-                                R.layout.item_recycler,
+                                R.layout.item_recyclerview,
                                 parent,
                                 false));
         return holder;
@@ -72,14 +133,27 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     @Override
     public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
 
-        holder.textView.setText(mList.get(position));
 
-        //修改选中item的颜色
-        if (mPosition == position) {
-            holder.textView.setBackgroundColor(mContext.getResources().getColor(R.color.colorPrimaryDark));
-        } else {
-            holder.textView.setBackgroundColor(mContext.getResources().getColor(R.color.white));
+        if (getItemViewType(position) == TYPE_NORMAL) {
+            if (holder instanceof MyViewHolder) {
+                holder.textView.setText(mList.get(position - 1));
+                //修改选中item的颜色
+                if (mPosition == position) {
+                    holder.textView.setBackgroundColor(mContext.getResources().getColor(R.color.colorPrimaryDark));
+                } else {
+                    holder.textView.setBackgroundColor(mContext.getResources().getColor(R.color.white));
+                }
+                return;
+            }
+            return;
+        } else if (getItemViewType(position) == TYPE_HEADER) {
+            holder.headerTextView.setText("----我是头部header----");
+            return;
+        } else if (getItemViewType(position) == TYPE_FOOTER) {
+            holder.footerTextView.setText("++++我是尾部footer+++++");
+            return;
         }
+
 
     }
 
@@ -88,15 +162,35 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
      */
     @Override
     public int getItemCount() {
-        return mList.size();
+        if (mHeaderView == null && mFooterView == null) {
+            return mList.size();
+        } else if (mHeaderView == null && mFooterView != null) {
+            return mList.size() + 1;
+        } else if (mHeaderView != null && mFooterView == null) {
+            return mList.size() + 1;
+        } else {
+            return mList.size() + 2;
+        }
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView textView;
+        TextView textView, headerTextView, footerTextView;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            /**
+             * 如果是headerView或者是footerView,直接返回
+             * */
+            if (itemView == mHeaderView) {
+                headerTextView = mHeaderView.findViewById(R.id.headerTextView);
+                return;
+            }
+            if (itemView == mFooterView) {
+                footerTextView = mFooterView.findViewById(R.id.footerTextView);
+                return;
+            }
             textView = itemView.findViewById(R.id.tv);
         }
     }
