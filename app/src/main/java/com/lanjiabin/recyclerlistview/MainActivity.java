6a50325615cef64b1aefebf925b7b05802f642a0
lanjiabin <1323457707@qq.com>
2020-02-20 15:36:52 +0800

    数据的修改和点击item的背景颜色变化。 数据的增加、删除、修改: 1.用户选择一个item，该item的颜色会变化; 2.当按点击add按钮时就会在该item上方添加一个数据; 3.长按该item会删除该数据; 4.点change按钮时会改变该item的数据。

--- app/src/main/java/com/lanjiabin/recyclerlistview/MainActivity.java
+++ app/src/main/java/com/lanjiabin/recyclerlistview/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -18,36 +19,57 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerAdapter mRecyclerAdapter;
     private List<String> mList = new ArrayList<>();
+    private Button addDataButton, changeData;
+    //记录用户选择了RecyclerListView哪个item
+    private int mPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initRecyclerView();
     }
 
     public void initView() {
+        addDataButton = findViewById(R.id.addData);
+        changeData = findViewById(R.id.changeData);
+        mRecyclerView = findViewById(R.id.recyclerListView);
+
+        addDataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //添加数据到选择item的上方
+                mRecyclerAdapter.addData(mPosition);
+            }
+        });
+
+
+        changeData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //修改选中item的数据
+                mRecyclerAdapter.changeData(mPosition);
+            }
+        });
+
+
+    }
+
+    public void initRecyclerView() {
+
         //添加测试数据
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 110; i++) {
             mList.add(i + " ");
         }
-        //布局关联
-        mRecyclerView = findViewById(R.id.recyclerListView);
         //设置布局方向-默认布局
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         //设置item增加和删除时的动画，这里设置默认动画
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
         //1.添加默认分割线：高度为2px，颜色为灰色
         mRecyclerView.addItemDecoration(new RecycleViewDivider(this, LinearLayoutManager.HORIZONTAL));
-//
-//        //2.添加自定义分割线：可自定义分割线drawable
-//        mRecyclerView.addItemDecoration(new RecycleViewDivider(
-//                this, LinearLayoutManager.HORIZONTAL, R.drawable.recyclerview_divideritem));
-//
-//        //3.添加自定义分割线：可自定义分割线高度和颜色
-//        mRecyclerView.addItemDecoration(new RecycleViewDivider(
-//                this, LinearLayoutManager.HORIZONTAL, 3, getResources().getColor(R.color.colorPrimaryDark)));
 
         //设置adapter关联的list
         mRecyclerAdapter = new RecyclerAdapter(this, mList);
@@ -56,17 +78,28 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.addOnItemTouchListener(new RecyclerViewClickListener(this, mRecyclerView, new RecyclerViewClickListener.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                Toast.makeText(MainActivity.this,"单击了："+position,Toast.LENGTH_SHORT).show();
+
+                //颜色改变了，更新Adapter
+                mRecyclerAdapter.notifyDataSetChanged();
+
+                //记录用户选择了哪个item
+                mPosition = position;
+
+                //传递点击了哪个item，以此用来更新选中item的颜色
+                mRecyclerAdapter.getCurrentPosition(position);
+
+                Toast.makeText(MainActivity.this, "单击了：" + position, Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
             public void onItemLongClick(View view, int position) {
-                Toast.makeText(MainActivity.this,"长击了："+position,Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "长击了：" + position, Toast.LENGTH_SHORT).show();
+                //删除长按的item
+                mRecyclerAdapter.removeData(position);
             }
         }));
 
-        //数据更新
-        mRecyclerAdapter.notifyDataSetChanged();
         //关联adapter
         mRecyclerView.setAdapter(mRecyclerAdapter);
     }
--- app/src/main/java/com/lanjiabin/recyclerlistview/RecyclerAdapter.java
+++ app/src/main/java/com/lanjiabin/recyclerlistview/RecyclerAdapter.java
@@ -16,21 +16,43 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
     private List<String> mList;
     private Context mContext;
 
+    //记录用户选择了RecyclerListView哪个item
+    //赋值-1是为了防止默认选中position=0的item
+    private int mPosition = -1;
+
     public RecyclerAdapter(Context context, List<String> list) {
         this.mContext = context;
         this.mList = list;
     }
 
+    //获取当前点击item的position
+    public void getCurrentPosition(int position) {
+        this.mPosition = position;
+    }
+
+    //移除数据
     public void removeData(int position) {
         mList.remove(position);
         notifyItemRemoved(position);
+    }
+
+    //新增数据
+    public void addData(int position) {
+        mList.add(position, "Add One" + position);
+        notifyItemInserted(position);
+    }
+
+    //更改某个位置的数据
+    public void changeData(int position) {
+        mList.set(position, "Item " + position + " has changed");
+        notifyItemChanged(position);
     }
 
     /**
      * 该方法会在RecyclerView需要展示一个item的时候回调，
      * 重写该方法，使ViewHolder加载item的布局，布局复用，提高性能，
      * 就ListView的优化一样，只不过RecyclerView把这个集成到官方方法中了。
-     * */
+     */
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -46,16 +68,24 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
 
     /**
      * 该方法是填充绑定item数据的
-     * */
+     */
     @Override
     public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
 
         holder.textView.setText(mList.get(position));
+
+        //修改选中item的颜色
+        if (mPosition == position) {
+            holder.textView.setBackgroundColor(mContext.getResources().getColor(R.color.colorPrimaryDark));
+        } else {
+            holder.textView.setBackgroundColor(mContext.getResources().getColor(R.color.white));
+        }
+
     }
 
     /**
      * 该方法是返回item的数量。
-     * */
+     */
     @Override
     public int getItemCount() {
         return mList.size();
